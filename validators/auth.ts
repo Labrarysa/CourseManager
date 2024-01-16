ff3e56236b0a51889a67090f7e577d65a43c2058
HassanAlabdulal <hhhalabdulaal@gmail.com>
2024-01-17 00:39:58 +0300

    feat: change the font

--- validators/auth.ts
+++ validators/auth.ts
@@ -18,4 +18,17 @@ export const registerSchema = z.object({
   year: z.string().min(1, { message: "يجب اختيار الصف الدراسي " }).max(30),
   password: z.string().min(6, { message: "يجب أن تحتوي كلمة المرور على ٧ ارقام على الأقل" }).max(100),
   confirmPassword: z.string().min(6,  { message: "يجب أن تحتوي كلمة المرور على ٧ ارقام على الأقل" }).max(100),
+//   fatherName: z.string()
+//   .min(7, { message: "اكتب اسمك الثلاثي كاملاً" })
+//   .max(255),
+//       fatherEmail: z.string().email({
+//         message:"الإيميل المدخل غير صحيح"
+//       }),
+//       fatherPhoneNumber:z
+//       .string()
+//       .min(10, {message:"يجب أن يحتوي السجل المدني على ١٠ أرقام وباللغة الإنجليزية فقط"})
+//       .max(10)
+//       .refine((val) => !isNaN(val as unknown as number), {
+//         message: "يجب أن يحتوي السجل المدني على أرقام فقط وباللغة الإنجليزية",
+//       }),
 });
