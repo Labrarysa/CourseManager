8bf1ad3717a3d205cc29099d37b2101ddf020c0b
Ali Al-Shaikh <mi3lix9@outlook.sa>
2024-01-17 22:36:20 +0000

    test

--- utils/supabse/middleware.ts
+++ utils/supabse/middleware.ts
@@ -3,7 +3,7 @@ import { type CookieOptions, createServerClient } from "@supabase/ssr";
 import { type NextRequest, NextResponse } from "next/server";
 
 export const createClient = (request: NextRequest) => {
-  // Create an unmodified response
+  // Create an unmodified response.
   let response = NextResponse.next({
     request: {
       headers: request.headers,
