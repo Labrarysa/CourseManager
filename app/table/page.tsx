140c6d5c237b3b7712138e854170c5e57d948f90
Ali Al-Shaikh <mi3lix9@outlook.sa>
2024-01-18 15:59:04 +0000

    add todo :)

--- app/table/page.tsx
+++ app/table/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import {
   Table,
   TableBody,
@@ -18,7 +18,7 @@ const initialStudents = [
     studentId: "12647593073",
     year: "الأول الابتدائي",
     email: "Hsoonxx@gmail.com",
-    phone: "0559763547"
+    phone: "0559763547",
   },
   {
     number: 2,
@@ -26,7 +26,7 @@ const initialStudents = [
     studentId: "758303736480",
     year: "الثالث ثانوي",
     email: "aboood@gmail.com",
-    phone: "0559307583"
+    phone: "0559307583",
   },
 ];
 
@@ -35,53 +35,56 @@ export default function Home() {
   const [students, setStudents] = useState(initialStudents);
   // Calculate the number of students for display in the table caption
   const studentCount = students.length;
+  // TODO: Use React Query to fetch data from the db instead of muck data.
 
   return (
     // Positioning the table in the center of the page
     <div className="absolute -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
       {/* Scrollable container for the table on smaller screens */}
       <div className="table-auto md:w-full w-[300px]">
-      {/* Table for displaying student data */}
-      <Table className="min-w-full text-center">
-        {/* Table caption showing the total number of students */}
-        <TableCaption className="text-xl font-semibold py-2">
-          {`عدد الطلاب: ${studentCount}`}
-        </TableCaption>
+        {/* Table for displaying student data */}
+        <Table className="min-w-full text-center">
+          {/* Table caption showing the total number of students */}
+          <TableCaption className="text-xl font-semibold py-2">
+            {`عدد الطلاب: ${studentCount}`}
+          </TableCaption>
 
-        {/* Table header with column titles */}
-        <TableHeader className="text-sm text-center">
-          {/* Row for the main title spanning all columns */}
-          <TableRow className="text-xl font-semibold py-2">
-            <TableHead colSpan={6} className="text-center">كشف أسماء الطلاب</TableHead>
-          </TableRow>
-
-          {/* Row for column headings */}
-          <TableRow>
-            <TableHead className="text-center">الرقم</TableHead>
-            <TableHead className="text-center">اسم الطالب الثلاثي</TableHead>
-            <TableHead className="text-center">السجل المدني</TableHead>
-            <TableHead className="text-center">المرحلة الدراسية</TableHead>
-            <TableHead className="text-center">البريد الالكتروني</TableHead>
-            <TableHead className="text-center">رقم الجوال</TableHead>
-          </TableRow>
-        </TableHeader>
-
-        {/* Table body with data rows */}
-        <TableBody className="divide-y">
-          {students.map((student) => (
-            // Row for each student's data
-            <TableRow className="text-sm text-center" key={student.number}>
-              <TableCell className="py-2 px-4">{student.number}</TableCell>
-              <TableCell className="py-2 px-4">{student.name}</TableCell>
-              <TableCell className="py-2 px-4">{student.studentId}</TableCell>
-              <TableCell className="py-2 px-4">{student.year}</TableCell>
-              <TableCell className="py-2 px-4">{student.email}</TableCell>
-              <TableCell className="py-2 px-4">{student.phone}</TableCell>
+          {/* Table header with column titles */}
+          <TableHeader className="text-sm text-center">
+            {/* Row for the main title spanning all columns */}
+            <TableRow className="text-xl font-semibold py-2">
+              <TableHead colSpan={6} className="text-center">
+                كشف أسماء الطلاب
+              </TableHead>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </div>
+
+            {/* Row for column headings */}
+            <TableRow>
+              <TableHead className="text-center">الرقم</TableHead>
+              <TableHead className="text-center">اسم الطالب الثلاثي</TableHead>
+              <TableHead className="text-center">السجل المدني</TableHead>
+              <TableHead className="text-center">المرحلة الدراسية</TableHead>
+              <TableHead className="text-center">البريد الالكتروني</TableHead>
+              <TableHead className="text-center">رقم الجوال</TableHead>
+            </TableRow>
+          </TableHeader>
+
+          {/* Table body with data rows */}
+          <TableBody className="divide-y">
+            {students.map((student) => (
+              // Row for each student's data
+              <TableRow className="text-sm text-center" key={student.number}>
+                <TableCell className="py-2 px-4">{student.number}</TableCell>
+                <TableCell className="py-2 px-4">{student.name}</TableCell>
+                <TableCell className="py-2 px-4">{student.studentId}</TableCell>
+                <TableCell className="py-2 px-4">{student.year}</TableCell>
+                <TableCell className="py-2 px-4">{student.email}</TableCell>
+                <TableCell className="py-2 px-4">{student.phone}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
     </div>
   );
 }
