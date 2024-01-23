91011ed4297e2c237c44ca646306106814f9064c
l3b3al <alabdulaal.alii@gmail.com>
2024-01-23 10:50:07 +0300

    enhance: Added more details to groups + adding them inside drawer

--- app/circles/page.tsx
+++ app/circles/page.tsx
@@ -17,20 +17,93 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { group } from "console";
 
 const circles = [
-  { id: 1, name: "الحلقة الأولى", groups: [{ id: 1, groupName: "a" }] },
-  { id: 2, name: "الحلقة الثانية", groups: [{ id: 1, groupName: "a" }] },
-  { id: 3, name: "الحلقة الثالثة", groups: [{ id: 1, groupName: "a" }] },
-  { id: 4, name: "الحلقة الرابعة", groups: [{ id: 1, groupName: "a" }] },
-  { id: 5, name: "الحلقة الخامسة", groups: [{ id: 1, groupName: "a" }] },
-  { id: 6, name: "الحلقة السادسة", groups: [{ id: 1, groupName: "a" }] },
-  { id: 7, name: "الحلقة السابعة", groups: [{ id: 1, groupName: "a" }] },
-  { id: 8, name: "الحلقة الثامنة", groups: [{ id: 1, groupName: "a" }] },
-  { id: 9, name: "الحلقة التاسعة", groups: [{ id: 1, groupName: "a" }] },
-  { id: 10, name: "الحلقة العاشرة", groups: [{ id: 1, groupName: "a" }] },
-  { id: 11, name: "الحلقة الحادية عشر", groups: [{ id: 1, groupName: "a" }] },
-  { id: 12, name: "الحلقة الثانية عشر", groups: [{ id: 1, groupName: "a" }] },
+  {
+    id: 1,
+    name: "الحلقة الأولى",
+    groups: [
+      { id: 1, groupName: "المصطفى", teacher: "أحمد محسن", studentCount: 9 },
+    ],
+  },
+  {
+    id: 2,
+    name: "الحلقة الثانية",
+    groups: [
+      { id: 1, groupName: "الأمير", teacher: "أحمد محسن", studentCount: 11 },
+    ],
+  },
+  {
+    id: 3,
+    name: "الحلقة الثالثة",
+    groups: [
+      { id: 1, groupName: "المجتبى", teacher: "أحمد محسن", studentCount: 7 },
+    ],
+  },
+  {
+    id: 4,
+    name: "الحلقة الرابعة",
+    groups: [
+      { id: 1, groupName: "الشهيد", teacher: "أحمد محسن", studentCount: 13 },
+    ],
+  },
+  {
+    id: 5,
+    name: "الحلقة الخامسة",
+    groups: [
+      { id: 1, groupName: "السجاد", teacher: "أحمد محسن", studentCount: 6 },
+    ],
+  },
+  {
+    id: 6,
+    name: "الحلقة السادسة",
+    groups: [
+      { id: 1, groupName: "الباقر", teacher: "أحمد محسن", studentCount: 9 },
+    ],
+  },
+  {
+    id: 7,
+    name: "الحلقة السابعة",
+    groups: [
+      { id: 1, groupName: "الصادق", teacher: "أحمد محسن", studentCount: 10 },
+    ],
+  },
+  {
+    id: 8,
+    name: "الحلقة الثامنة",
+    groups: [
+      { id: 1, groupName: "الكاظم", teacher: "أحمد محسن", studentCount: 15 },
+    ],
+  },
+  {
+    id: 9,
+    name: "الحلقة التاسعة",
+    groups: [
+      { id: 1, groupName: "الرضا", teacher: "أحمد محسن", studentCount: 6 },
+    ],
+  },
+  {
+    id: 10,
+    name: "الحلقة العاشرة",
+    groups: [
+      { id: 1, groupName: "الجواد", teacher: "أحمد محسن", studentCount: 9 },
+    ],
+  },
+  {
+    id: 11,
+    name: "الحلقة الحادية عشر",
+    groups: [
+      { id: 1, groupName: "الهادي", teacher: "أحمد محسن", studentCount: 11 },
+    ],
+  },
+  {
+    id: 12,
+    name: "الحلقة الثانية عشر",
+    groups: [
+      { id: 1, groupName: "العسكري", teacher: "أحمد محسن", studentCount: 12 },
+    ],
+  },
 ];
 
 const Circles = () => {
@@ -49,7 +122,7 @@ const Circles = () => {
         {circles.map((circle) => (
           <Drawer key={circle.id}>
             <DrawerTrigger asChild>
-              <Card className="flex flex-col col-span-1 text-center transition duration-500 transform bg-white divide-y divide-gray-200 shadow cursor-pointer hover:scale-105 hover:shadow-md">
+              <Card className="text-center transition duration-500 transform shadow cursor-pointer hover:scale-105 hover:shadow-md">
                 <CardHeader>
                   <CardTitle className="">{circle.name}</CardTitle>
                   <CardDescription>
@@ -58,7 +131,30 @@ const Circles = () => {
                 </CardHeader>
               </Card>
             </DrawerTrigger>
-            <DrawerContent>Hi</DrawerContent>
+            <DrawerContent>
+              <div className="flex flex-col items-center justify-center m-8">
+                <DrawerHeader>
+                  <DrawerTitle>مجموعات {circle.name}</DrawerTitle>
+                </DrawerHeader>
+                <div
+                  role="list"
+                  className="grid grid-cols-1 gap-8 m-8 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4"
+                >
+                  {circle.groups.map((group) => (
+                    <Card
+                      key={group.id}
+                      className="text-center transition duration-500 transform shadow cursor-pointer hover:scale-105 hover:shadow-md"
+                    >
+                      <CardHeader>
+                        <CardTitle>{group.groupName}</CardTitle>
+                        <CardDescription>المحاضر: {group.teacher}</CardDescription>
+                        <CardDescription>عدد الطلاب: {group.studentCount}</CardDescription>
+                      </CardHeader>
+                    </Card>
+                  ))}
+                </div>
+              </div>
+            </DrawerContent>
           </Drawer>
         ))}
       </div>
