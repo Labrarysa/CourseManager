3b8d72f4b0caace2a4e75c5083aa84aa6cc67852
l3b3al <alabdulaal.alii@gmail.com>
2024-01-23 08:41:52 +0300

    enhance: Renaming circles, adding groups & UI changes

--- app/circles/page.tsx
+++ app/circles/page.tsx
@@ -11,40 +11,57 @@ import {
   DrawerTitle,
   DrawerTrigger,
 } from "@/components/ui/drawer";
+import {
+  Card,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 const circles = [
-  { id: 1, name: "A" },
-  { id: 2, name: "B" },
-  { id: 3, name: "C" },
-  { id: 4, name: "D" },
-  { id: 5, name: "E" },
-  { id: 6, name: "F" },
-  { id: 7, name: "G" },
-  { id: 8, name: "H" },
-  { id: 9, name: "I" },
-  { id: 10, name: "J" },
-  { id: 11, name: "K" },
-  { id: 12, name: "L" },
+  { id: 1, name: "الحلقة الأولى", groups: [{ id: 1, groupName: "a" }] },
+  { id: 2, name: "الحلقة الثانية", groups: [{ id: 1, groupName: "a" }] },
+  { id: 3, name: "الحلقة الثالثة", groups: [{ id: 1, groupName: "a" }] },
+  { id: 4, name: "الحلقة الرابعة", groups: [{ id: 1, groupName: "a" }] },
+  { id: 5, name: "الحلقة الخامسة", groups: [{ id: 1, groupName: "a" }] },
+  { id: 6, name: "الحلقة السادسة", groups: [{ id: 1, groupName: "a" }] },
+  { id: 7, name: "الحلقة السابعة", groups: [{ id: 1, groupName: "a" }] },
+  { id: 8, name: "الحلقة الثامنة", groups: [{ id: 1, groupName: "a" }] },
+  { id: 9, name: "الحلقة التاسعة", groups: [{ id: 1, groupName: "a" }] },
+  { id: 10, name: "الحلقة العاشرة", groups: [{ id: 1, groupName: "a" }] },
+  { id: 11, name: "الحلقة الحادية عشر", groups: [{ id: 1, groupName: "a" }] },
+  { id: 12, name: "الحلقة الثانية عشر", groups: [{ id: 1, groupName: "a" }] },
 ];
 
 const Circles = () => {
   return (
     <main>
-      <h1>الحلقات</h1>
+      <div className="m-8">
+        <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+          الحلقات
+        </h1>
+      </div>
       <Separator />
-      <ul
+      <div
         role="list"
-        className="grid grid-cols-1 gap-8 mx-8 sm:gap-12 sm:mx-12 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4"
+        className="grid grid-cols-1 gap-8 m-8 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4"
       >
         {circles.map((circle) => (
-          <li
-            key={circle.id}
-            className="flex flex-col col-span-1 text-center transition duration-500 transform bg-white divide-y divide-gray-200 rounded-lg shadow cursor-pointer hover:scale-105 hover:shadow-lg"
-          >
-            {circle.name}
-          </li>
+          <Drawer key={circle.id}>
+            <DrawerTrigger asChild>
+              <Card className="flex flex-col col-span-1 text-center transition duration-500 transform bg-white divide-y divide-gray-200 shadow cursor-pointer hover:scale-105 hover:shadow-md">
+                <CardHeader>
+                  <CardTitle className="">{circle.name}</CardTitle>
+                  <CardDescription>
+                    عدد المجموعات: {circle.groups.length}
+                  </CardDescription>
+                </CardHeader>
+              </Card>
+            </DrawerTrigger>
+            <DrawerContent>Hi</DrawerContent>
+          </Drawer>
         ))}
-      </ul>
+      </div>
     </main>
   );
 };
