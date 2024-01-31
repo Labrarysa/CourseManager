f2e99de6f47318e01d3ce49ef65e0b3416fe54a0
l3b3al <alabdulaal.alii@gmail.com>
2024-01-31 20:54:13 +0300

    feat: Enable adding groups to circles using dialog + UI enhancement

--- app/groups/page.tsx
+++ app/groups/page.tsx
@@ -1,4 +1,7 @@
 import React from "react";
+import { Plus } from "lucide-react";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
 import { Separator } from "@/components/ui/separator";
 import {
@@ -16,8 +19,20 @@ import {
   CardDescription,
   CardHeader,
   CardTitle,
+  CardContent,
 } from "@/components/ui/card";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+
 import { group } from "console";
+import { Button } from "@/components/ui/button";
 
 const circles = [
   {
@@ -134,27 +149,68 @@ const Circles = () => {
             <DrawerContent>
               <div className="flex flex-col items-center justify-center m-8">
                 <div>
-                <DrawerHeader>
-                  <DrawerTitle>مجموعات {circle.name}</DrawerTitle>
-                </DrawerHeader>
+                  <DrawerHeader>
+                    <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">
+                      مجموعات {circle.name}
+                    </h3>
+                  </DrawerHeader>
                 </div>
-                
-                <div
-                  role="list"
-                  className="flex justify-center"
-                >
-                  {circle.groups.map((group) => (
-                    <Card
-                      key={group.id}
-                      className="text-center transition duration-500 transform shadow cursor-pointer hover:scale-105 hover:shadow-md"
-                    >
-                      <CardHeader>
-                        <CardTitle>{group.groupName}</CardTitle>
-                        <CardDescription>المحاضر: {group.teacher}</CardDescription>
-                        <CardDescription>عدد الطلاب: {group.studentCount}</CardDescription>
-                      </CardHeader>
-                    </Card>
-                  ))}
+                <div className="flex justify-center">
+                  <div
+                    role="list"
+                    className="grid grid-cols-1 gap-8 m-8 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4"
+                  >
+                    {circle.groups.map((group) => (
+                      <Card
+                        key={group.id}
+                        className="text-center transition duration-500 transform shadow cursor-pointer hover:scale-105 hover:shadow-md"
+                      >
+                        <CardHeader>
+                          <CardTitle>{group.groupName}</CardTitle>
+                          <CardDescription>
+                            المحاضر: {group.teacher}
+                          </CardDescription>
+                          <CardDescription>
+                            عدد الطلاب: {group.studentCount}
+                          </CardDescription>
+                        </CardHeader>
+                      </Card>
+                    ))}
+                    <Dialog>
+                      <DialogTrigger>
+                        <Card className="flex flex-col h-full items-center justify-center transition duration-500 transform shadow cursor-pointer border-2 border-dashed hover:scale-105 hover:shadow-md">
+                          <CardContent className="p-0">
+                            <Plus size={48}></Plus>
+                          </CardContent>
+                        </Card>
+                      </DialogTrigger>
+                      <DialogContent>
+                        <DialogHeader>
+                          <DialogTitle>
+                            إضافة مجموعة جديدة إلى {circle.name}
+                          </DialogTitle>
+                        </DialogHeader>
+                        <DialogDescription>
+                          قم بتعبئة النموذج لإضافة مجموعة جديدة
+                        </DialogDescription>
+                        <div className="grid gap-4 py-4">
+                          <div className="grid grid-cols-4 items-center gap-4">
+                            <Label htmlFor="name" className="text-left">
+                              اسم المجموعة
+                            </Label>
+                            <Input
+                              id="name"
+                              value=""
+                              className="col-span-3"
+                            />
+                          </div>
+                        </div>
+                        <DialogFooter>
+                          <Button type="submit">حفظ</Button>
+                        </DialogFooter>
+                      </DialogContent>
+                    </Dialog>
+                  </div>
                 </div>
               </div>
             </DrawerContent>
