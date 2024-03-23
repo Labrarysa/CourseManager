6ffe172002a958d454cc0adb518b0f1bdca68307
Ali Al-Shaikh <mi3lix9@outlook.sa>
2024-03-23 14:50:57 +0000

    create 100 fake students

--- db/seed.ts
+++ db/seed.ts
@@ -1,9 +1,29 @@
+import { Faker, ar } from '@faker-js/faker';
 import { Student, db, } from 'astro:db';
 
 // https://astro.build/db/seed
 export default async function seed() {
-	await db.insert(Student).values([
-		{ firstName: "Ali", lastName: "Al-Shaikh", id: "1" },
-		{ firstName: "Hassan", lastName: "", id: "2" }
-	])
+	// Generate random 10 students
+	await db.insert(Student).values(Array.from({ length: 100 }, () => createRandomStudent()))
+}
+
+
+const faker = new Faker({
+	locale: [ar],
+});
+
+/**
+ * Create a random student data.
+ * Warning: It is not typed yet.
+ * @returns student
+ */
+function createRandomStudent() {
+	return {
+		id: faker.string.uuid(),
+		firstName: faker.person.firstName("male"),
+		fatherName: faker.person.firstName("male"),
+		grandFatherName: faker.person.firstName("male"),
+		lastName: faker.person.lastName(),
+		birthDate: faker.date.between({ from: new Date(2000), to: new Date(2015) })
+	}
 }
