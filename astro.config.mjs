4408eb18b0bf4fcb76be7ae62b99f33bda6b67ac
HassanAlabdulal <hhhalabdulaal@gmail.com>
2024-03-27 11:24:58 +0300

    add: install Tailwind CSS

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config';
 import db from "@astrojs/db";
 import react from "@astrojs/react";
-
 import vercel from "@astrojs/vercel/serverless";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [db(), react()],
+  integrations: [db(), react(), tailwind()],
   output: "server",
   adapter: vercel()
 });
