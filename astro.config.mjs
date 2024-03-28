6441b6c99a0031a6a398ca9b93e76b147e6b58bd
HassanAlabdulal <hhhalabdulaal@gmail.com>
2024-03-29 01:40:18 +0300

    add: install Shadcn UI

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import db from "@astrojs/db";
 import react from "@astrojs/react";
 import vercel from "@astrojs/vercel/serverless";
@@ -7,7 +7,13 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [db(), react(), tailwind()],
+  integrations: [
+    db(),
+    react(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+  ],
   output: "server",
-  adapter: vercel()
+  adapter: vercel(),
 });
