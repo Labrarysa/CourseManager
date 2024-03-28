77675320a92552f2146b4b376e00771205fd8d72
HassanAlabdulal <hhhalabdulaal@gmail.com>
2024-03-29 01:44:11 +0300

    add: install Shadcn components

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -20,7 +20,8 @@ const buttonVariants = cva(
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
-        default: "h-10 px-4 py-2",
+        default: "h-8 px-5 py-2",
+
         sm: "h-9 rounded-md px-3",
         lg: "h-11 rounded-md px-8",
         icon: "h-10 w-10",
@@ -33,6 +34,7 @@ const buttonVariants = cva(
   }
 );
 
+
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
     VariantProps<typeof buttonVariants> {
@@ -42,6 +44,7 @@ export interface ButtonProps
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, variant, size, asChild = false, ...props }, ref) => {
     const Comp = asChild ? Slot : "button";
+
     return (
       <Comp
         className={cn(buttonVariants({ variant, size, className }))}
