a6cb92b42e19dd10709673874e3dd228131a852d
Ali Al-Shaikh <mi3lix9@outlook.sa>
2024-03-28 23:38:48 +0000

    fix

--- db/config.ts
+++ db/config.ts
@@ -24,30 +24,6 @@ const Group = defineTable({
     id: column.text({ primaryKey: true }),
     name: column.text(),
     circle: column.number({ references: () => Circle.columns.number }),
-    year: column.number()
-  }
-})
-
-const StudentGroup = defineTable({
-  columns: {
-    studentId: column.text({ references: () => Student.columns.id }),
-    groupId: column.text({ references: () => Group.columns.id })
-  },
-})
-
-
-const Circle = defineTable({
-  columns: {
-    number: column.number({ primaryKey: true })
-  }
-
-})
-
-const Group = defineTable({
-  columns: {
-    id: column.text({ primaryKey: true }),
-    name: column.text(),
-    circle: column.number({ references: () => Circle.columns.number }),
     year: column.number(),
     teacherId: column.text({ references: () => Teacher.columns.id, optional: true })
   }
@@ -70,7 +46,7 @@ const Teacher = defineTable({
 
 // https://astro.build/db/config
 export default defineDb({
-  tables: { Student },
+  tables: { Student, Circle, Group, StudentGroup, Teacher },
 });
 
 
